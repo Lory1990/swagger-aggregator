87914144a895d64acb1d1fa967998c9b7631f570
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-03-05 12:26:02 +0100

    Rifattorizza il monitoraggio dei pod per utilizzare una callback e gestire i namespace

--- src/plugins/HookOnPodsPlugin.ts
+++ src/plugins/HookOnPodsPlugin.ts
@@ -4,6 +4,32 @@ import { FastifyInstance } from 'fastify';
 import KubernetesSwaggerDiscoveryService from '../services/KubernetesSwaggerDiscoveryService.js';
 import { fastifyApp, inMemoryCache } from '../index.js';
 
+const watchCallback = (startDate: Date, type : string, pod : any) => {
+        if(Date.now() - startDate.getTime() < 30000) return
+        if (!pod || !pod.metadata || !pod.status) return
+
+          const owner = pod.metadata.ownerReferences?.find((o : any) => o.kind === "ReplicaSet");
+          if (!owner) return; // Il pod non ha un owner di tipo ReplicaSet
+    
+          const replicaSetName = owner.name;
+    
+          // Recupera il deployment dal nome del ReplicaSet
+          const deploymentName = replicaSetName.replace(/-\w{9,10}$/, ""); // Rimuove l'ID univoco del ReplicaSet
+
+          if(['Pending', 'Failed'].includes(pod.status.phase)) return;
+    
+          const cacheKey = `deployment:${deploymentName}`;
+          if (inMemoryCache.get(cacheKey)) {
+              fastifyApp.log.info(`Ignorato cambiato di stato: ${pod.status.phase} per ${deploymentName}, già registrato recentemente`);
+            return;
+          }
+    
+          inMemoryCache.set(cacheKey, true);
+    
+          fastifyApp.log.info(`Pod ${pod.metadata.name} cambiato di stato: ${pod.status.phase} per deployment ${deploymentName}`);
+          
+          new KubernetesSwaggerDiscoveryService().discovery();
+}
 
 export default fastifyPlugin(async (fastify: FastifyInstance) => {
         
@@ -11,40 +37,20 @@ export default fastifyPlugin(async (fastify: FastifyInstance) => {
         kc.loadFromDefault(); 
         
         const watch = new k8s.Watch(kc);
-        const namespaces = process.env.NAMESPACES ? process.env.NAMESPACES.split(",") : undefined
-
+        const namespaces = process.env.NAMESPACES ? process.env.NAMESPACES.split(",") : undefined        
         const date = new Date();
         fastifyApp.log.info("Start watching pods");
-        watch.watch("/api/v1/pods", { watch: true, resourceVersion: "0" },
-                (type, pod) => {
-                  if(Date.now() - date.getTime() < 30000) return
-                  if (!pod || !pod.metadata || !pod.status) return
-                  if(namespaces && !namespaces.includes(pod.metadata.namespace)) return
-
-                    const owner = pod.metadata.ownerReferences?.find((o : any) => o.kind === "ReplicaSet");
-                    if (!owner) return; // Il pod non ha un owner di tipo ReplicaSet
-              
-                    const replicaSetName = owner.name;
-              
-                    // Recupera il deployment dal nome del ReplicaSet
-                    const deploymentName = replicaSetName.replace(/-\w{9,10}$/, ""); // Rimuove l'ID univoco del ReplicaSet
-
-                    if(['Pending', 'Failed'].includes(pod.status.phase)) return;
-              
-                    const cacheKey = `deployment:${deploymentName}`;
-                    if (inMemoryCache.get(cacheKey)) {
-                        fastifyApp.log.info(`Ignorato cambiato di stato: ${pod.status.phase} per ${deploymentName}, già registrato recentemente`);
-                      return;
-                    }
-              
-                    inMemoryCache.set(cacheKey, true);
-              
-                    fastifyApp.log.info(`Pod ${pod.metadata.name} cambiato di stato: ${pod.status.phase} per deployment ${deploymentName}`);
-                    
-                    new KubernetesSwaggerDiscoveryService().discovery();
-                },
-                (err) => {
-                  fastify.log.error(err);
-                }
-        );
+        if(namespaces){
+                for(const namespace of namespaces){
+                        watch.watch(`/api/v1/namespaces/${namespace}/pods`, { watch: true, resourceVersion: "0" },
+                                (type, pod) => watchCallback(date, type, pod),
+                                (err) => fastify.log.error(err)
+                        );
+        }
+        }else{
+                watch.watch("/api/v1/pods", { watch: true, resourceVersion: "0" },
+                        (type, pod) => watchCallback(date, type, pod),
+                        (err) => fastify.log.error(err)
+                );
+        }
 });
